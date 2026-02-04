37090970c67ae4a04831fe30c115e44df925a799
Slaiminus <ender200510@gmail.com>
2026-02-04 21:03:22 +0300

    Ураа полностью готова постройка кормушек

--- Assets/Scripts/FeederCreator.cs
+++ Assets/Scripts/FeederCreator.cs
@@ -16,17 +16,14 @@ public class FeederCreator : MonoBehaviour
     [SerializeField] private Button CreateFeeder;
     [SerializeField] private Button CloseMenu;
     [SerializeField] private Button TestOpenButton;
-
-    [SerializeField] private GameObject feeder1;
-    [SerializeField] private GameObject feeder2;
-    [SerializeField] private GameObject feeder3;
-
+    [SerializeField] private GameObject Panel; 
     [SerializeField] private GameObject[] _Feeders;
 
     private int Counter = 0;
     private bool isOpen = false;
+    public bool isTargeted = false;
 
-   // private RectTransform rectTransform;
+    // private RectTransform rectTransform;
     private void Awake()
     {
         ButtonFeeder1.onClick.AddListener(() => feederCounter(1));
@@ -34,16 +31,17 @@ public class FeederCreator : MonoBehaviour
         ButtonFeeder3.onClick.AddListener(() => feederCounter(3));
         CreateFeeder.onClick.AddListener(Create);
         CloseMenu.onClick.AddListener(Close);
-        TestOpenButton.onClick.AddListener(Open);
+        // TestOpenButton.onClick.AddListener(Open);
     }
-    void Update()
-    {
-        //uiElement.anchoredPosition
-    }
-
     private void Create() 
     {
-        Debug.Log($"Создана кормушка {Counter}");
+        if (isTargeted)
+        {
+            Debug.Log($"Создана кормушка {Counter}");
+            Instantiate(_Feeders[Counter - 1], transform.parent);
+            Close();
+            Destroy(gameObject);
+        }
     }
     private void feederCounter(int number)
     {
@@ -52,11 +50,12 @@ public class FeederCreator : MonoBehaviour
     private void Open()
     {
         isOpen = true;
-        transform.DOMoveX(1550, 1);
+        Panel.transform.DOMoveX(1550, 1);
     }
     private void Close()
     {
         isOpen = false;
-        transform.DOMoveX(2500, 1);
+        isTargeted = false;
+        Panel.transform.DOMoveX(2500, 1);
     }
 }
--- Assets/Scripts/TreeTeleporter.cs
+++ Assets/Scripts/TreeTeleporter.cs
@@ -1,12 +1,20 @@
 using DG.Tweening;
 using System.Security.Cryptography.X509Certificates;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class TreeTeleporter : MonoBehaviour //Скрипт закреплён на Camera Center
 {
     [SerializeField] private string TreeTag;
     [SerializeField] private string FeederTag;
+    [SerializeField] private string GhostTag;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject Panel;
+
+    // private FeederCreator feederCreator;
+    private GameObject objectHit;
+    private GameObject lastObject;
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -21,15 +29,42 @@ public class TreeTeleporter : MonoBehaviour //Скрипт закреплён на Camera Center
 
         if (Physics.Raycast(ray, out hit))
         {
-            if (hit.transform.CompareTag(TreeTag))
+            if (objectHit != null)
             {
-                transform.DOMove(hit.transform.position + new Vector3(0, -3, 0), moveSpeed);
+                lastObject = objectHit;
+            }
+            objectHit = hit.collider.gameObject;
+            
+
+            if (objectHit.CompareTag(TreeTag))
+            {
+                transform.DOMove(objectHit.transform.position + new Vector3(0, -3, 0), moveSpeed);
+                lastObject.GetComponent<FeederCreator>().isTargeted = false;
+                Close();
             }
 
-            if (hit.transform.CompareTag(FeederTag))
+            if (objectHit.CompareTag(FeederTag))
             {
-                transform.DOMove(hit.transform.position, moveSpeed);
+                transform.DOMove(objectHit.transform.position, moveSpeed);
+                lastObject.GetComponent<FeederCreator>().isTargeted = false;
+                Close();
+            }
+
+            if (objectHit.CompareTag(GhostTag))
+            {
+                transform.DOMove(objectHit.transform.position, moveSpeed);
+                objectHit.GetComponent<FeederCreator>().isTargeted = true;
+                Open();
             }
         }
+    }
+
+    private void Open()
+    {
+        Panel.transform.DOMoveX(1550, 1);
+    }
+    private void Close()
+    {
+        Panel.transform.DOMoveX(2500, 1);
     }
 }
